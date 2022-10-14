3b698eafa10294487a44b49ca4e5e213f569e69c
swvmpdad <will.g.camp@gmail.com>
2022-10-14 11:38:40 -0500

    added CSS and Header and Footer components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Made with love by me.
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <h1>
+                Garrett Campbell
+            </h1>
+
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#about-me">About Me</a>
+                    </li>
+
+                    <li>
+                        <a href="#my-projects">My Projects</a>
+                    </li>
+
+                    <li>
+                        <a href="#contact-me">Contact Me</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
