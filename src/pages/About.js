23b92d1640faba8fc4aee304e19cf3cf54cde5e5
swvmpdad <will.g.camp@gmail.com>
2022-10-16 14:49:37 -0500

    fixed styling on Portfolio page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,13 +2,11 @@ import React from "react";
 
 const About = () => {
     return (
-        <section id="about-me" className="about-me">
-                <div className="section-title">
+        <section id="about-me" className="container">
+                <div className="flex-row section-title">
                 <h2>About Me</h2>
                 </div>
-                <div className="about">
-                    <div>
-                    </div>
+                <div className="flex-row about-me">
                     <div>
                     <p>I am a 34 year old dad from Southeast Texas.
                         I have spent most of my life being fascinated with computers and technology.
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,72 +2,75 @@ import React from "react";
 
 const Portfolio = () => {
     return (
-        <section id="my-projects" className="my-projects">
+        <section>
                 <div className="section-title">
                 <h2>My Projects</h2>
                 </div>
+                <section id="my-projects" className="my-projects">
                     <section className="projects">
-                        <a href="https://swvmpdad.github.io/run-buddy/">
                             <div className="project" id="run-buddy">
-                                
                                 <figure className="project-name">
                                     <h3>Run Buddy</h3>
                                     <p>
                                         HTML/CSS
                                     </p>
+                                    <a href="https://swvmpdad.github.io/run-buddy/">Deployed</a>
+                                    <a href="">Git Repo</a>
                                 </figure>
                             </div>
-                        </a>
-                      <a href="https://swvmpdad.github.io/taskinator/">
                         <div className="project" id="taskinator">
                             <figure className="project-name">
                                 <h3>Taskinator</h3>
                                 <p>
                                     JavaScript
                                 </p>
+                                <a href="https://swvmpdad.github.io/taskinator/">Deployed</a>
+                                <a href="">Git Repo</a>
                             </figure>
                         </div>
-                      </a>
-                      <a href="https://swvmpdad.github.io/robot-gladiators/">
+                      
                         <div className="project" id="battle-robot">
                             <figure className="project-name">
                                 <h3>Robot Gladiators</h3>
                                 <p>
                                     JavaScript
                                 </p>
+                                <a href="https://swvmpdad.github.io/robot-gladiators/">Deployed</a>
+                                <a href="">Git Repo</a>
                             </figure>
                         </div>
-                      </a>
-                      <a href="https://swvmpdad.github.io/workin-hard/">
                         <div className="project" id="workin-hard">
                             <figure className="project-name">
                                 <h3>Workin Hard</h3>
                                 <p>
                                     JavaScript
                                 </p>
+                                <a href="https://swvmpdad.github.io/workin-hard/">Deployed</a>
+                                <a href="">Git Repo</a>
                             </figure>
                         </div>
-                      </a>
-                      <a href="https://swvmpdad.github.io/showbiz-baby/">
                         <div className="project" id="showbiz-baby">
                             <figure className="project-name">
                                 <h3>Showbiz Baby</h3>
                                 <p>
                                     HTML, JavaScript, Bulma, CSS
                                 </p>
+                                <a href="https://swvmpdad.github.io/showbiz-baby/">Deployed</a>
+                                <a href="">Git Repo</a>
                             </figure>
                         </div>
-                      </a>
-                      <a href="https://social-strength.herokuapp.com">
+
                         <div className="project" id="social-strength">
                             <figure className="project-name">
                                 <h3>Social Strength</h3>
                                 <p>
                                     MySQL, JavaScript, Handlebars, Node.js
                                 </p>
+                                <a href="https://social-strength.herokuapp.com">Deployed</a>
+                                <a href="">Git Repo</a>
                             </figure>
                         </div>
-                      </a>
+                    </section>
                     </section>
         </section>
     );
