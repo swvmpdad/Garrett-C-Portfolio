e8f9169a1758eea1c6b4a149cbd6e5dbf6f6a2a7
swvmpdad <will.g.camp@gmail.com>
2022-10-17 14:18:58 -0500

    coded resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,16 +1,44 @@
 import React from "react";
+import Resume from "../components/Resume";
 
 const ResumePage = () => {
+    const skills = [
+        "CSS",
+        "HTML5",
+        "JavaScript",
+        "React",
+        "OOP",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+        "NoSQL",
+        "MySQL",
+        "APIs",
+        "jQuery",
+        "Bootstrap",
+        "Git",
+        "GitHub",
+        "Heroku"
+    ]
     return (
         <section>
             <div className="section-title">
-                <h2>My Resume</h2>
+                <h2>My Resume and Skills</h2>
             </div>
             <div id="resume" className="row text-center mt-3">
-                <Resume />
+                <Resume /><br />
+                <p className="mt-3">I am proficient in the following areas:</p><br />
+                <ul>
+                    {skills.map((skill) => {
+                            return (
+                                <li>{skill}</li>
+                            );
+                        }
+                    )}
+                </ul>
             </div>
         </section>
-    )
-}
+    );
+};
 
 export default ResumePage;
